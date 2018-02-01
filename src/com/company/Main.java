44bb1bcb8f83200a7911c922c43d654f7b9ee476
Vaidotas <vaidotas.eigelis@gmail.com>
2018-02-01 11:37:59 +0200

    Day 3 Code 3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,6 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         float suma = 0;
         float skaicius = 0;
 
@@ -14,7 +13,6 @@ public class Main {
             System.out.print("Ivesk skaiciu: ");
             skaicius = sc.nextFloat();
             suma += skaicius;
-
 
         } while (skaicius != 0);
         System.out.println("Suma: " + suma);
